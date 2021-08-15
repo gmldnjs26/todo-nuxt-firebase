4f8a9890d1021f2ae80aeb0fe4a0fef58fcb5891
gmldnjs26 <imleesky265@gmail.com>
2021-08-15 17:37:56 +0900

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-blue-500 h-[350px]">
+    asd
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
---
+++ src/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/components/**/*.{vue,js}',
+    './src/layouts/**/*.vue',
+    './src/pages/**/*.vue',
+    './src/plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
