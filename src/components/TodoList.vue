17a4b60bd796eca5ee3a681c4003827e262744fb
gmldnjs26 <imleesky265@gmail.com>
2021-08-26 22:53:57 +0900

    create: todolist as composition-api

---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    TodoList
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@nuxtjs/composition-api'
+
+type Todo = {
+  doDate: string
+  context: string
+  completion: boolean
+  createdAt: string
+  userId: string
+}
+
+export default defineComponent({
+  props: {
+    todoList: {
+      type: Array as PropType<Todo[]>,
+      required: false,
+      default: () => []
+    }
+  },
+  setup (_, context) {
+    console.log(context)
+  }
+})
+</script>
+
+<style>
+
+</style>
