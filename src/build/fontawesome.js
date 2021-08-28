dc40274b6877aca275a901246cdcf79d2c1c42bf
gmldnjs26 <imleesky265@gmail.com>
2021-08-28 18:56:15 +0900

    config: fontawesome

---
+++ src/build/fontawesome.js
@@ -0,0 +1,18 @@
+const solid = [
+  'faCaretLeft',
+  'faChartLine',
+  'faCheckCircle',
+  'faChevronDown',
+  'faChevronLeft',
+  'faChevronRight',
+  'faDownload',
+  'faEdit',
+  'faUpload',
+  'faUser'
+]
+
+const regular = ['faStar']
+
+const brands = ['faTwitter']
+
+export { solid, regular, brands }
