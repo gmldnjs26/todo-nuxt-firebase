bd201107a5f9a20c946872ef2282bd03ae9ba9b0
gmldnjs26 <imleesky265@gmail.com>
2021-08-28 18:56:45 +0900

    test: fontawesome

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="bg-blue-500 h-[350px]">
+    <font-awesome-layers class="fa-4x">
+      <font-awesome-icon icon="download" />
+    </font-awesome-layers>
     This is Index Page
   </div>
 </template>
