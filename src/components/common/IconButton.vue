1302a3bcca48dda7888991687ef8f66c7a5a88a4
gmldnjs26 <imleesky265@gmail.com>
2021-09-02 21:27:00 +0900

    feat: Add IconButton component

---
+++ src/components/common/IconButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button>
+    <font-awesome-icon :icon="icon" :class="styles" />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String,
+      required: false,
+      default: 'text-xl'
+    }
+  },
+  emits: ['click'],
+  setup (props) {
+    const styles = computed(() => {
+      const classes = []
+      classes.push(props.size)
+      return classes.join(' ')
+    })
+
+    return {
+      styles
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
