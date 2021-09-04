c492c09b2dd07ebbed1e8c6c3b09e5599988f7e2
gmldnjs26 <imleesky265@gmail.com>
2021-09-04 19:16:28 +0900

    add: todoEditModal

---
+++ src/components/todo/TodoEditModal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="fixed inset-0 w-full h-full bg-black opacity-50 z-10" />
+    <div class="absolute w-[350px] h-[350px] m-auto bg-blue-500 z-20" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup () {
+
+  }
+})
+</script>
