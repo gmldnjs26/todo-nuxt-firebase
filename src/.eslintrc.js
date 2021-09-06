0debc661b79d777cd8e8b70bf4bc1b0db44105e5
gmldnjs26 <imleesky265@gmail.com>
2021-09-06 21:37:09 +0900

    config: eslint prettier

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -2,16 +2,10 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
+  plugins: [],
   // add your custom rules here
-  rules: {
-    printWidth: 120
-  }
+  rules: {},
 }
