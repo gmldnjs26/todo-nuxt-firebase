42500b1e61090918bbdd8db29ddd308ea386a71a
gmldnjs26 <imleesky265@gmail.com>
2021-09-08 22:22:51 +0900

    fix: TodoEditModal style

--- src/components/todo/TodoEditModal.vue
+++ src/components/todo/TodoEditModal.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <div class="fixed inset-0 w-full h-full bg-black opacity-50 z-10" @click="$emit('toggleTodoEditModal')" />
-    <div class="absolute w-[350px] h-[350px] m-auto flex flex-col bg-blue-500 z-20">
-      <div>
+    <div class="absolute w-[350px] h-[250px] m-auto flex flex-col bg-white z-20 rounded-md p-3">
+      <div class="h-20 text-2xl flex items-center justify-center">
         title
       </div>
-      <div class="flex-1 flex">
-        <div>
-          <IconButton icon="edit" />
-          <div>
-            수정
+      <div class="flex flex-wrap">
+        <div v-for="(item, i) in TODO_EDIT_EVENTS" :key="i" class="flex flex-col items-center justify-center m-3">
+          <IconButton class="w-10 h-10 rounded-full p-1 bg-gray-200 hover:bg-gray-400" :icon="item.event" />
+          <div class="text-sm mt-1">
+            {{ item.title }}
           </div>
         </div>
       </div>
@@ -19,13 +19,16 @@
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
+import { TODO_EDIT_EVENTS } from '@/utils/const'
 
 export default defineComponent({
   components: {
-    IconButton: () => import('@/components/common/IconButton.vue')
+    IconButton: () => import('@/components/common/IconButton.vue'),
   },
-  setup () {
-
-  }
+  setup() {
+    return {
+      TODO_EDIT_EVENTS,
+    }
+  },
 })
 </script>
