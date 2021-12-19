976a2c4db7fd26b3c4c24db1569149d37872add9
gmldnjs26 <imleesky265@gmail.com>
2021-12-19 15:50:00 +0900

    feat: radio component

---
+++ src/components/RadioButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <label class="inline-flex items-center">
+    <input
+      type="radio"
+      :name="name"
+      class="mr-1 w-4 h-4 form-radio"
+      :value="value"
+      :disabled="isDisabled"
+      @click="click(value)"
+    />
+    <span :class="`${isChecked ? 'text-primary' : 'text-gray-500'}`">{{ text }}</span>
+  </label>
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    value: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    text: {
+      type: String,
+      required: false,
+      default: 'asd',
+    },
+    color: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    isChecked: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    isDisabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  setup(props, { emit }) {
+    const click = (value: string | number): string | number | void => {
+      if (props.isDisabled) return
+      emit('click', value)
+    }
+    return {
+      click,
+    }
+  },
+})
+</script>
