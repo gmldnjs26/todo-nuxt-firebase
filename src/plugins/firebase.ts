cf80a7cb6654c0a2bf91ac48a44573698dbf7b69
gmldnjs26 <imleesky265@gmail.com>
2022-02-21 21:03:17 +0900

    fix: firebase bug

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -9,17 +9,15 @@ import { Plugin } from '@nuxt/types'
 
 // const firebaseConfig = FirebaseConfigInfo
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.FIREBASE_APIKEY,
-    authDomain: process.env.FIREBASE_AUTHDOMAIN,
-    projectId: process.env.FIREBASE_PROJECTID,
-    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.FIREBASE_APPID,
-    measurementId: process.env.FIREBASE_MEASURMENTID,
-  })
-}
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASURMENTID,
+})
 
 declare module 'vue/types/vue' {
   interface Vue {
