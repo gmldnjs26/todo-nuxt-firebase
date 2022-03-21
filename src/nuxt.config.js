60e156576063e23c0205849cb41339c7b149607b
gmldnjs26 <imleesky265@gmail.com>
2022-03-21 14:17:01 +0900

    for ci test

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -28,7 +28,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/firebase.ts', ssr: false },
+    // { src: '~/plugins/firebase.ts', ssr: false },
     // サーバーサイドでは使用できないのでssrはfalseにすること
     { src: '~/plugins/persistedstate.js', ssr: false },
   ],
