583538157f75f9260e645384f6e0543da979e16c
gmldnjs26 <imleesky265@gmail.com>
2022-07-11 23:37:39 +0900

    feat: add modal

--- src/components/CategoryManage.vue
+++ src/components/CategoryManage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="text-right">
+    <CategoryManageModal />
     <button class="bg-primary_three hover:bg-primary_two text-white text-sm font-bold py-2 px-4 rounded transition-all">
       카테고리 관리
     </button>
@@ -8,8 +9,10 @@
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
+import CategoryManageModal from './CategoryManageModal.vue'
 
 export default defineComponent({
+  components: { CategoryManageModal },
   setup() {},
 })
 </script>
