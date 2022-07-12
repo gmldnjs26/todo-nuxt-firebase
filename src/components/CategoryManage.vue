f72f317a3bb2d8d0202d4d004575e4b72a0acce8
gmldnjs26 <imleesky265@gmail.com>
2022-07-12 23:14:09 +0900

    fix: style

--- src/components/CategoryManage.vue
+++ src/components/CategoryManage.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="text-right">
-    <CategoryManageModal />
-    <button class="bg-primary_three hover:bg-primary_two text-white text-sm font-bold py-2 px-4 rounded transition-all">
-      카테고리 관리
-    </button>
+  <div>
+    <CategoryManageModal v-show="isShowManageModal" />
+    <div class="text-right">
+      <button
+        type="button"
+        class="bg-primary_three hover:bg-primary_two text-white text-sm font-bold py-2 px-4 rounded transition-all"
+        @click="toogleManageModal"
+      >
+        카테고리 관리
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 import CategoryManageModal from './CategoryManageModal.vue'
 
 export default defineComponent({
   components: { CategoryManageModal },
-  setup() {},
+  setup() {
+    const isShowManageModal = ref(false)
+    const toogleManageModal = () => {
+      isShowManageModal.value = !isShowManageModal.value
+    }
+    return {
+      isShowManageModal,
+      toogleManageModal,
+    }
+  },
 })
 </script>
 
--- src/components/CategoryManageModal.vue
+++ src/components/CategoryManageModal.vue
@@ -1,24 +1,19 @@
 <template>
   <div>
     <div class="fixed top-0 left-0 z-10 w-screen h-screen bg-black opacity-40"></div>
-    <div class="fixed inset-0 z-10 m-auto bg-white shadow-md animate-appear w-[500px] max-h-[500px]">
+    <div class="fixed inset-0 z-10 m-auto bg-white shadow-md animate-appear rounded-md w-[370px] max-h-[500px]">
       <div class="flex overflow-auto flex-col w-full h-full">
         <!-- header -->
         <div class="flex justify-between items-center px-5 h-16 text-sm font-semibold bg-bgColor-basic">
-          <label class="text-gray-700">エリアから検索</label>
+          <label class="text-gray-700">카테고리 관리</label>
         </div>
         <!-- /header -->
 
         <!-- body -->
-        <div class="overflow-auto flex-1 px-5 mt-5 w-full h-full">asdasd</div>
+        <div class="overflow-auto flex-1 px-5 mt-5 w-full h-full">body</div>
 
         <!-- footer -->
-        <div class="flex justify-between items-center px-4 h-20 bg-opacity-10 border-t-2 border-gray-200 ring-white">
-          <div>
-            <span class="text-sm font-bold">{{ areaCount }}エリア</span>
-            <span class="text-xs">選択中</span>
-          </div>
-        </div>
+        <div class="flex justify-between items-center px-4 h-20 bg-opacity-10 border-t-2 border-gray-200 ring-white">footer</div>
         <!-- /footer -->
       </div>
     </div>
